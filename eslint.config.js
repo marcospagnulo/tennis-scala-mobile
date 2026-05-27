fbefb9434a53049f3d7cf64c8b6d77239269ea30
Marco Spagnulo <marco.spagnulo@cetma.it>
2026-05-27 08:39:12 +0200

    fix config

--- eslint.config.js
+++ eslint.config.js
@@ -32,7 +32,6 @@ export default defineConfig([
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "warn",
-      //https://react.dev/reference/eslint-plugin-react-hooks/lints/set-state-in-effect
       "react-refresh/only-export-components": "off",
       "react-hooks/set-state-in-effect": "off",
       "react/jsx-uses-react": "off",
