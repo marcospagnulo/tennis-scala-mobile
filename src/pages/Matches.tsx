235b0e1844d5f3ca54d7d379bd97697057816a9d
Marco Spagnulo <marco.spagnulo@cetma.it>
2026-06-01 09:05:04 +0200

    remove match page

--- src/pages/Matches.tsx
+++
@@ -1,6 +0,0 @@
-import type {Theme} from "@emotion/react";
-import {Stack, type SxProps} from "@mui/material";
-
-export function MatchesPage({sx}: {sx?: SxProps<Theme>}) {
-  return <Stack sx={{...sx}}></Stack>;
-}
