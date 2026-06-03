e0b005a1744cca46db08cdba36e5b95d7bb7b09a
Marco Spagnulo <marco.spagnulo@cetma.it>
2026-06-03 10:32:42 +0200

    delete ranking player in player info dialog

--- src/pages/ranking/row/PlayerInfo.tsx
+++ src/pages/ranking/row/PlayerInfo.tsx
@@ -1,9 +1,9 @@
 import {
   Avatar,
-  Box,
   Button,
   CircularProgress,
   Dialog,
+  DialogActions,
   DialogContent,
   Stack,
   Typography,
@@ -13,9 +13,10 @@ import {collections} from "../../../lib/firebase";
 import {useEffect, useState} from "react";
 import dayjs from "dayjs";
 import {doc, getDoc} from "firebase/firestore";
-import {Refresh, ReportProblem} from "@mui/icons-material";
+import {Delete, Refresh, ReportProblem} from "@mui/icons-material";
 import {useRefreshRanking} from "../../../functions/ranking/useRefreshRanking";
 import {useAppContext} from "../../../app/context";
+import {useDeleteRanking} from "../../../functions/ranking/useDeleteRanking";
 
 const RowData = ({label, value}: {label: string; value?: string}) => (
   <Stack direction="row" spacing={1}>
@@ -34,9 +35,32 @@ const PlayerInfo = ({
   onClose: () => void;
 }) => {
   const [player, setPlayer] = useState<Player>();
-  const [loading, setLoading] = useState(true);
-  const {currentSeason} = useAppContext();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [deleting, setDeleting] = useState<boolean>(false);
+
+  const {currentSeason, user} = useAppContext();
+  const isAdmin = user?.role === "admin";
   const {loading: refreshLoading, refreshRanking} = useRefreshRanking();
+  const {loading: deleteLoading, deleteRanking} = useDeleteRanking();
+
+  const handleClose = () => {
+    setTimeout(() => {
+      setPlayer(undefined);
+      setLoading(true);
+      setDeleting(false);
+    }, 300);
+    onClose();
+  };
+
+  const handleDelete = () => {
+    setDeleting(true);
+  };
+
+  const handleConfirmDelete = () => {
+    deleteRanking(currentSeason!, ranking);
+    setDeleting(false);
+    handleClose();
+  };
 
   useEffect(() => {
     if (!collections || !open) {
@@ -63,59 +87,75 @@ const PlayerInfo = ({
 
   const handleRefresh = () => {
     refreshRanking(currentSeason!, ranking);
-    onClose();
+    handleClose();
   };
 
   return (
-    <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
-      <DialogContent
-        sx={{height: "50vh", display: "flex", flexDirection: "column"}}>
-        {loading && (
-          <Stack sx={{alignItems: "center", justifyContent: "center", flex: 1}}>
-            <CircularProgress />
-          </Stack>
+    <>
+      <Dialog open={open} onClose={handleClose} maxWidth="sm" fullWidth>
+        <DialogContent
+          sx={{height: "50vh", display: "flex", flexDirection: "column"}}>
+          {loading && (
+            <Stack
+              sx={{alignItems: "center", justifyContent: "center", flex: 1}}>
+              <CircularProgress />
+            </Stack>
+          )}
+          {!loading && player && (
+            <Stack spacing={2}>
+              <Avatar
+                src={player.avatar}
+                sx={{width: 150, height: 150, alignSelf: "center"}}
+              />
+              <Typography variant="h5" align="center">
+                {player.name} {player.surname}
+              </Typography>
+              <RowData label="Email" value={player.email} />
+              <RowData label="Telefono" value={player.phone} />
+              <RowData
+                label="Data di nascita"
+                value={
+                  player.birthDate
+                    ? dayjs(player.birthDate.toDate()).format("DD/MM/YYYY")
+                    : undefined
+                }
+              />
+            </Stack>
+          )}
+          {!loading && !player && (
+            <Stack
+              sx={{alignItems: "center", justifyContent: "center", flex: 1}}>
+              <ReportProblem color="error" sx={{width: 120, height: 120}} />
+              <Typography color="textPrimary" variant="h6" align="center">
+                Giocatore non trovato
+              </Typography>
+            </Stack>
+          )}
+        </DialogContent>
+        {isAdmin && (
+          <DialogActions>
+            <Button
+              variant="text"
+              size="small"
+              startIcon={<Refresh />}
+              disabled={refreshLoading}
+              onClick={handleRefresh}>
+              Ricarica
+            </Button>
+            <Button
+              variant={deleting ? "contained" : "outlined"}
+              size="small"
+              color="error"
+              sx={{ml: 2}}
+              startIcon={<Delete />}
+              onClick={deleting ? handleConfirmDelete : handleDelete}
+              disabled={deleteLoading}>
+              {deleting ? "Conferma" : "Elimina"}
+            </Button>
+          </DialogActions>
         )}
-        {!loading && player && (
-          <Stack spacing={2}>
-            <Avatar
-              src={player.avatar}
-              sx={{width: 150, height: 150, alignSelf: "center"}}
-            />
-            <Typography variant="h5" align="center">
-              {player.name} {player.surname}
-            </Typography>
-            <RowData label="Email" value={player.email} />
-            <RowData label="Telefono" value={player.phone} />
-            <RowData
-              label="Data di nascita"
-              value={
-                player.birthDate
-                  ? dayjs(player.birthDate.toDate()).format("DD/MM/YYYY")
-                  : undefined
-              }
-            />
-            <Box>
-              <Button
-                variant="contained"
-                size="small"
-                startIcon={<Refresh />}
-                disabled={refreshLoading}
-                onClick={handleRefresh}>
-                Ricarica dati anagrafici
-              </Button>
-            </Box>
-          </Stack>
-        )}
-        {!loading && !player && (
-          <Stack sx={{alignItems: "center", justifyContent: "center", flex: 1}}>
-            <ReportProblem color="error" sx={{width: 120, height: 120}} />
-            <Typography color="textPrimary" variant="h6" align="center">
-              Giocatore non trovato
-            </Typography>
-          </Stack>
-        )}
-      </DialogContent>
-    </Dialog>
+      </Dialog>
+    </>
   );
 };
 
